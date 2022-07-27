69b90b425852284f789e2a09ef255ff7a334f8fc
seba5197 <sebastian5197@gmail.com>
2022-07-27 12:54:23 -0400

    xx

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ try {
   if(payload){
  
     let nom = req.body.entry[0].changes[0].value.contacts[0].profile.name
-    let num = req.body.entry[0].changes[0].value;
+    let num = req.body.entry[0].changes[0].value.contacts[0].input;
 
     console.log(num+" entra ------------------->"+ nom +" selecciono "+payload)
     let data ={
