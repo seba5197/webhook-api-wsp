0b30784c7214cafa29e2ada0524df0ea3ffed372
seba5197 <sebastian5197@gmail.com>
2022-07-27 10:04:32 -0400

    xx

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 app.post('/facebook', function(req, res) {
   console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
   
-  const payload = req.body.entry[0].changes[0].value.messages[0].button.payload
+  //const payload = req.body.entry[0].changes[0].value.messages[0].button.payload
   const mensaje = req.body.entry[0].changes[0].value.messages[0].text.body
   const nombre = req.body.entry[0].changes[0].value.contacts[0].profile.name
   const numero = req.body.entry[0].changes[0].value.metadata.display_phone_number;
